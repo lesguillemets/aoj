4c886d88cc6cde89875d57aa7d125ae8d3fcddb8
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-22 00:12:00 +0900

    0058 -- runtime error?

---
+++ Volume00/0058.hs
@@ -0,0 +1,23 @@
+import Text.Read (readMaybe)
+import Data.Maybe (catMaybes)
+type Vect = (Double, Double)
+
+threshold :: Double
+threshold = 0.0000001
+
+isOrthog :: Vect -> Vect -> Bool
+isOrthog (x0,y0) (x1,y1) = abs (x0*x1+y0*y1) < threshold
+
+parseLn :: [Double] -> Maybe (Vect, Vect)
+parseLn [xa,ya,xb,yb,xc,yc,xd,yd] = Just ((xb-xa,yb-ya),(xd-xc,yd-yc))
+parseLn _ = Nothing
+
+addLeadingZeros :: String -> String
+addLeadingZeros s = if head s == '.' then '0':s else s
+
+main = getContents >>=
+    mapM_ (\l -> case parseLn . catMaybes . map (readMaybe . addLeadingZeros) .  words $ l of
+            Nothing -> return ()
+            Just c -> putStrLn . (\b -> if b then "YES" else "NO")
+                    . uncurry isOrthog $ c
+                    ) . lines
