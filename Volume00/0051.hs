746483f22ebf7b1443a0cd2d06ac447dd24403f8
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-27 22:09:04 +0900

    fix 0051 !

--- Volume00/0051.hs
+++ Volume00/0051.hs
@@ -2,7 +2,9 @@ import Control.Monad
 import Data.List
 
 toI :: String -> Int
-toI = read . dropWhile (== '0')
+toI s = let nums = dropWhile (== '0') s in
+    if null nums then 0
+                 else read s
 d :: String -> Int
 d s = (toI . sortBy (flip compare)) s - (toI . sort) s
 
