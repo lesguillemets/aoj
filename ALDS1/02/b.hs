64ee157902b6147d86ded7723419c0e450700a6b
lesguillemets <lesguillemets@users.noreply.github.com>
2016-01-11 21:50:42 +0900

    ALDS1/02/b

--- ALDS1/02/b.hs
+++ ALDS1/02/b.hs
@@ -2,15 +2,9 @@
 {-# LANGUAGE RankNTypes #-}
 import Control.Applicative
 
-selectMinimumBy :: (a -> a -> Ordering) -> [a] -> ([a],a,[a])
-selectMinimumBy f (x:xs) = smb ([],x,xs)
-    where
-        smb cand [] = ([],cand)
-        smb cand (y:ys) = case f cand y of
-                               LT -> y <:> smb cand ys
-                               _ -> cand <:> smb y ys
-selectMinimum :: Ord a => [a] -> ([a],a)
-selectMinimum = selectMinimumBy compare
+findMinimum :: Ord a => [a] -> (a,Int)
+findMinimum = minimum . flip zip [0..]
+
 
 selectionSort :: Ord a => [a] -> ([a],Int)
 selectionSort = selectionSort' . (,0)
@@ -24,11 +18,12 @@ selectionSort = selectionSort' . (,0)
 -- TODO : find a good higer order function>
 selectionSort' :: Ord a => ([a],Int) -> ([a],Int)
 selectionSort' ([],n) = ([],n)
-selectionSort' (xs,n) = let (rs,h) = selectMinimum xs
+selectionSort' (x:xs,n) = let (h,i) = findMinimum (x:xs)
+                              rs = take (i-1) xs ++ x:drop(i) xs
                             in
-                            h <:> if h /= head xs
+                            h <:> if i /= 0
                                      then selectionSort' (rs, n+1)
-                                     else selectionSort' (rs,n)
+                                     else selectionSort' (xs,n)
 -- |
 
 (<:>) x (xs,b) = (x:xs,b)
